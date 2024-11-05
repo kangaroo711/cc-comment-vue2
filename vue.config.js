586f30bc777372808add97b19203ba4730294fe8
hcc <huangchuancong@qq.com>
2024-11-05 16:55:51 +0800

    feat: 解决旧版本兼容警告

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   transpileDependencies: ["@dcloudio/uni-ui"],
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require("sass"),
+        sassOptions: {
+          // 静默弃用警告
+          silenceDeprecations: ["legacy-js-api", "import", "global-builtin"],
+        },
+      },
+    },
+  },
 };
